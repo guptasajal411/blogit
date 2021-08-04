bb8bec9d3b3ffd6398e0c719447ccb49591b4a18
Sajal Gupta <guptasajal411@gmail.com>
2021-08-04 20:47:38 +0530

    Init NPM

--- app.js
+++ app.js
@@ -12,22 +12,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(3000, function () {
+	console.log("Server started on port 3000");
 });
