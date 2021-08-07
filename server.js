d16493649ee72457b477f052749cd4d04ff64eb4
Sajal Gupta <guptasajal411@gmail.com>
2021-08-07 23:12:12 +0530

    Update environment port for Heroku

--- server.js
+++ server.js
@@ -59,6 +59,6 @@ app.get("/posts/:route", function(req, res){
 	res.redirect("/");
 })
 
-app.listen(3000, function () {
+app.listen(process.env.PORT || 3000, () => {
 	console.log("Server started on port 3000");
 });
